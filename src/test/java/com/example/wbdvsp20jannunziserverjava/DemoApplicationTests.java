a3df1770ce523881488517319623d21221d3aef9
jannunzi <jannunzi@gmail.com>
2020-01-11 07:43:38 -0500

    first commit

---
+++ src/test/java/com/example/wbdvsp20jannunziserverjava/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wbdvsp20jannunziserverjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
